369d55fdd092f46c280d5c6b35cd0c077b9040a9
Tradonix <d.m.andrzejewski94@gmail.com>
2020-06-23 14:17:51 +0200

    new apps

--- main/models.py
+++ main/models.py
@@ -1,3 +1,14 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class Subjects(models.Model):
+    name = models.CharField(max_length=32)
+    teacher = models.ForeignKey(User, on_delete=models.CASCADE)
+    students = models.ManyToManyField(User)
+
+
+class Grades(models.Model):
+    value = models.IntegerField()
+    subject = models.ForeignKey(Subjects, on_delete=models.CASCADE)
+    graded_by = models.ForeignKey(User, on_delete=models.CASCADE)
